39b79840a3c8a0f5d639b903ad32f1d06cbb3276
chaiwat20180 <chaiwat20180@gmail.com>
2025-02-04 16:31:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,128 @@
 function convertData() {
-  const inputData = $("#InputData").val().trim();
-  const selectedOption = $('input[name="options"]:checked').val();
-  const selectedCommand = $('input[name="selectCommand"]:checked').val();
-  let TextCommand = "";
+    const inputData = $("#InputData").val().trim();
+    const inputColumn = $("#InputColumn").val().trim();
+    const selectedOption = $('input[name="options"]:checked').val();
+    const selectedCommand = $('input[name="selectCommand"]:checked').val();
+    const selectedTypeInsert = $('input[name="TypeInsert"]:checked').val();
+    let TextCommand = "";
 
-  if (selectedCommand === 'SelectAll') {
-      TextCommand = "SELECT * FROM tbname WHERE columnname IN ";
-  } 
-  if (selectedCommand === 'UpdateAll') {
-      TextCommand = "UPDATE tbname SET columnname = '0' WHERE columnname IN ";
-  }
 
-  if (selectedOption === "GEN2QS" && inputData) {
-      const lines = inputData.split("\n");
-      const outputData = TextCommand + `('${lines.join("','")}')`;
-      $("#OutputData").val(outputData);
-  } 
-  else if (selectedOption === "GEN2QM" && inputData) {
-      const lines = inputData.split("\n");
-      const outputData = " " + `('${lines.join("','")}')`;
-      $("#OutputData").val(outputData);
-  } 
-  else if (selectedOption === "GEN2QL" && inputData) {
-      const lines = inputData.split("\n");
-      const outputData = `('${lines.join("'),('")}')`;
-      $("#OutputData").val(outputData);
-  } 
-  else if (selectedOption === "Update" && inputData) {
-      const lines = inputData.split("\n");
-      const outputData = TextCommand + `('${lines.join("','")}')`;
-      $("#OutputData").val(outputData);
-  } 
-  else {
-      $("#OutputData").val("Error");
-  }
+    if (selectedCommand === 'InsertData') {
+        $("#SelectTypeInsert").show();
+    } else {
+        $("#SelectTypeInsert").hide();
+    }
+    if(selectedTypeInsert === 'InsertManual'){
+        $("#InsertColumn").show();
+    }else{
+        $("#InsertColumn").hide();
+    }
+
+
+    if (selectedCommand === 'SelectAll') {
+        TextCommand = "SELECT * FROM tbname WHERE columnname IN ";
+    }
+    else if (selectedCommand === 'UpdateAll') {
+        TextCommand = "UPDATE tbname SET columnname = '0' WHERE columnname IN ";
+    }
+    else if (selectedCommand === 'InsertData' && selectedTypeInsert === 'InsertAll') {
+        TextCommand = "INSERT INTO tbname VALUES";
+    }
+    else if (selectedCommand === 'InsertData' && selectedTypeInsert === 'InsertManual') {
+        TextCommand = "INSERT INTO tbname";
+    }
+    else if (selectedCommand === 'DeleteData') {
+        TextCommand = "DELETE FROM tbname WHERE columnname IN  ";
+    }
+    else {
+        TextCommand = "";
+    }
+    if (selectedOption === "GEN2QS" && inputData) {
+        const lines = inputData.split("\n");
+        const linesColumn = inputColumn.split("\n");
+        let outputData = TextCommand + `('${lines.join("','")}')`;
+        if (selectedCommand === 'InsertData' && selectedTypeInsert === 'InsertManual') {
+            outputData = TextCommand + `('${linesColumn.join("','")}') VALUES` + `('${lines.join("','")}')`;
+        }
+
+        $("#OutputData").val(outputData);
+    }   
+    // else if (selectedOption === "GEN2QM" && inputData) {
+    //     const lines = inputData.split("\n");
+    //     const outputData = " " + `('${lines.join("','")}')`;
+    //     $("#OutputData").val(outputData);
+    // }
+    // else if (selectedOption === "GEN2QL" && inputData) {
+    //     const lines = inputData.split("\n");
+    //     const outputData = `('${lines.join("'),('")}')`;
+    //     $("#OutputData").val(outputData);
+    // }
+    // else if (selectedOption === "Update" && inputData) {
+    //     const lines = inputData.split("\n");
+    //     const outputData = TextCommand + `('${lines.join("','")}')`;
+    //     $("#OutputData").val(outputData);
+    // }
+    else {
+        $("#OutputData").val("Please Input Data");
+    }
 }
 
 function clear_data() {
-  $("#InputData").val("");
-  $("#OutputData").val("");
-  Swal.fire({
-      title: 'Success!',
-      text: 'Clear success!',
-      icon: 'success',
-      timer: 1500,
-      confirmButtonText: 'OK'
-  });
+    $("#InputData").val("");
+    $("#OutputData").val("");
+    Swal.fire({
+        title: 'Success!',
+        text: 'Clear success!',
+        icon: 'success',
+        timer: 1500,
+        confirmButtonText: 'OK'
+    });
 }
 
 function copy_data() {
-  const textArea = $("#OutputData");
-  const tempElement = $("<textarea></textarea>").val(textArea.val());
-  const customConfirmButton = Swal.mixin({
-      customClass: {
-          confirmButton: "btn btn-danger",
-          cancelButton: "btn btn-success"
-      },
-      buttonsStyling: true
-  });
+    const textArea = $("#OutputData");
+    const tempElement = $("<textarea></textarea>").val(textArea.val());
+    const customConfirmButton = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-danger",
+            cancelButton: "btn btn-success"
+        },
+        buttonsStyling: true
+    });
 
-  $("body").append(tempElement);
-  tempElement.select();
+    $("body").append(tempElement);
+    tempElement.select();
 
-  customConfirmButton.fire({
-      title: 'กำลังดำเนินการ...',
-      text: 'กรุณารอสักครู่',
-      allowOutsideClick: false,
-      showConfirmButton: false,
-      didOpen: () => {
-          customConfirmButton.showLoading();
-      }
-  });
+    customConfirmButton.fire({
+        title: 'กำลังดำเนินการ...',
+        text: 'กรุณารอสักครู่',
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        didOpen: () => {
+            customConfirmButton.showLoading();
+        }
+    });
 
-  navigator.clipboard.writeText(tempElement.val()).then(() => {
-      customConfirmButton.showLoading();
-      Swal.fire({
-          title: 'Success!',
-          text: 'Copy success!',
-          icon: 'success',
-          timer: 1500,
-          confirmButtonText: 'OK'
-      });
-  }).catch(err => {
-      //debug
-      console.error("Failed to copy:", err); 
-      Swal.fire({
-          title: 'Error!',
-          text: 'Copy Failed. Please try again or CTRL+F5 and try again.',
-          icon: 'error',
-          timer: 1500,
-          confirmButtonText: 'OK'
-      });
-  });
-  tempElement.remove();
+    navigator.clipboard.writeText(tempElement.val()).then(() => {
+        customConfirmButton.showLoading();
+        Swal.fire({
+            title: 'Success!',
+            text: 'Copy success!',
+            icon: 'success',
+            timer: 1500,
+            confirmButtonText: 'OK'
+        });
+    }).catch(err => {
+        //debug
+        console.error("Failed to copy:", err);
+        Swal.fire({
+            title: 'Error!',
+            text: 'Copy Failed. Please try again or CTRL+F5 and try again.',
+            icon: 'error',
+            timer: 1500,
+            confirmButtonText: 'OK'
+        });
+    }).finally(() => {
+        tempElement.remove();
+    });
 }
