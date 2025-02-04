77068e7a17086b300db44594a2ad18faa85bb2e7
chaiwat20180 <chaiwat20180@gmail.com>
2025-02-04 10:38:53 +0700

    Update versions.js
    Add new update and modify detail

--- versions.js
+++ versions.js
@@ -1,22 +1,41 @@
 const versionData = [
     {
+        "version": "1.0.4",
+        "releaseDate": "2025-02-04",
+        "features": [
+                        "Change some class for support Mobile",
+                        "Add Title and Change to Label (Input Data & Export Data)",
+                        "Add RGB Title Version Update",
+                        "Add Copyright"
+                    ]
+    },
+    {
         "version": "1.0.3",
         "releaseDate": "2025-02-03 Hotfix",
-        "features": ["Convert JS to Jquery"]
+        "features": [
+                        "Convert JS to Jquery"
+                    ]
     },
     {
         "version": "1.0.2",
         "releaseDate": "2025-02-03",
-        "features": ["Add Show list None & SELECT & UPDATE","Add Dark Mode", "Add Ver. Detail"]
+        "features": [
+                        "Add Show list None & SELECT & UPDATE",
+                        "Add Dark Mode", "Add Ver. Detail"
+                    ]
     },
     {
         "version": "1.0.1", 
         "releaseDate": "2024-08-12",
-        "features": ["Add Function Button Copy & Clear "]
+        "features": [
+                        "Add Function Button Copy & Clear "
+                    ]
     },
     {
         "version": "1.0.0",
         "releaseDate": "2024-07-02",
-        "features": ["Support GEN2QS Function"]
+        "features": [
+                        "Support GEN2QS Function"
+                    ]
     }
 ];
